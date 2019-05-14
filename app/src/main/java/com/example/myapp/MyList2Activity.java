d951095f5208e0d3d5336cde1e2c2ac948f0f65c
Yvonne <237313360@qq.com>
2019-05-14 20:34:43 +0800

    11.0

--- app/src/main/java/com/example/myapp/MyList2Activity.java
+++ app/src/main/java/com/example/myapp/MyList2Activity.java
@@ -1,6 +1,8 @@
 package com.example.myapp;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -20,11 +22,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class MyList2Activity extends ListActivity implements Runnable,AdapterView.OnItemClickListener {
+public class MyList2Activity extends ListActivity implements Runnable,AdapterView.OnItemClickListener,AdapterView.OnItemLongClickListener {
 
     private static final String TAG = "MyList2Activity";
     Handler handler;
-    private ArrayList<HashMap<String,String>> listItems; //存放文字、图片信息
+    private List<HashMap<String,String>> listItems; //存放文字、图片信息
     private SimpleAdapter listItemAdapter; //适配器
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +44,12 @@ public class MyList2Activity extends ListActivity implements Runnable,AdapterVie
             @Override
             public void handleMessage(Message msg){
                 if(msg.what==7){
-                    List<HashMap<String,String>> list2 = (List<HashMap<String,String>>) msg.obj;
-                    //ListAdapter adapter = new ArrayAdapter<String>(MyList2Activity.this,R.layout.support_simple_spinner_dropdown_item);
+                    listItems = (List<HashMap<String,String>>) msg.obj;
+                    //ListAdapter adapter = new ArrayAdapter<String>(MyList2Activity.this,
+                    // R.layout.support_simple_spinner_dropdown_item);
+
                     //生成适配器的item和动态数组对应的元素
-                    listItemAdapter = new SimpleAdapter(MyList2Activity.this,list2,//listitem数据源
+                    listItemAdapter = new SimpleAdapter(MyList2Activity.this,listItems,//listitem数据源
                             R.layout.activity_my_list2, //listitem的XML布局实现
                             new String[]{"ItemT","ItemD"},new int[]{R.id.itemT,R.id.itemD});
                     setListAdapter(listItemAdapter);
@@ -53,7 +57,8 @@ public class MyList2Activity extends ListActivity implements Runnable,AdapterVie
                 super.handleMessage(msg);
             }
         };
-        getListView().setOnItemClickListener(this); //点击list一栏，跳转下一个页面
+        getListView().setOnItemClickListener(this); //点击list一栏，跳转下一个页面 短按
+        getListView().setOnItemLongClickListener(this); //长按
     }
 
     private void initListView(){
@@ -114,5 +119,28 @@ public class MyList2Activity extends ListActivity implements Runnable,AdapterVie
         rateCalc.putExtra("title",title);
         rateCalc.putExtra("rate",Float.parseFloat(detail));
         startActivity(rateCalc);
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+        Log.i(TAG, "onItemLongClick: 长按position = " + position);
+
+        //构造对话框，进行确认操作
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("提示").setMessage("请确认是否删除当前数据栏").
+                setPositiveButton("是", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i(TAG, "onClick: 对话框事件处理");
+                        //删除操作
+                        listItems.remove(position); //删除数据
+                        listItemAdapter.notifyDataSetChanged(); //刷新
+                    }
+                }).setNegativeButton("否",null);
+
+        builder.create().show();
+
+        Log.i(TAG, "onItemLongClick: size = " + listItems.size());
+        return true;
     }
 }
--- app/src/main/java/com/example/myapp/MyListActivity.java
+++ app/src/main/java/com/example/myapp/MyListActivity.java
@@ -2,22 +2,42 @@ package com.example.myapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
-public class MyListActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MyListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    List<String> data = new ArrayList<String>();
+    private String TAG = "MyList";
+    ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_list);
         //自己建立列表
         ListView listView = findViewById(R.id.mylist);
-        String data[] = {"one","two","","go"};
+        // init data 初始化数据
+        for(int i=0;i<10;i++){
+            data.add("item" + i);
+        }
 
-        ListAdapter adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_expandable_list_item_1,data);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,data);
         listView.setAdapter(adapter);
+        listView.setEmptyView(findViewById(R.id.nodata)); //在没有数据时
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i(TAG, "onItemClick: position = " + position);
+        Log.i(TAG, "onItemClick: parent = " + parent);
+        adapter.remove(parent.getItemAtPosition(position));
+        //adapter.notifyDataSetChanged(); //刷新
     }
 }
